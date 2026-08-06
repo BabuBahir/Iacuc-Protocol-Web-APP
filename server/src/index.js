8b932a2c7cfc5fe0e349e10ffb4fcae9181342ee
rb <rohit.logicsquare@gmail.com>
2026-08-06 11:36:04 +0530

    Change PORT variable to use process.env.PORT

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { createApp } from "./app.js";
 
 const app = createApp();
-const PORT = process.env.API_BASE_URL || 4000;
+const PORT = process.env.PORT  || 4000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`IACUC API listening on port ${PORT}`);
