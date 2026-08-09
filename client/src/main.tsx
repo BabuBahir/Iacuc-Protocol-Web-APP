d56bfcdfccdab06fd779385526f618bf609d73ab
babu bahir <rohit.logicsquare@gmail.com>
2026-08-09 22:44:36 +0530

    Gate Vercel Analytics to production builds only

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Analytics />
+      {import.meta.env.PROD && <Analytics />}
     </BrowserRouter>
   </React.StrictMode>
 );
